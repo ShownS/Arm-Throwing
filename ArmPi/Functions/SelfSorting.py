03dc1dc709ecf46e25bac5c8483515f2791cbecd
shownm <shownm@oregonstate.edu>
2026-03-12 12:58:37 -0700

    test

--- ArmPi/Functions/SelfSorting.py
+++ ArmPi/Functions/SelfSorting.py
@@ -499,9 +499,11 @@ if __name__ == '__main__':
                 mode = input("Basket? y/n")
 
                 if mode =="y":
+                    print("Going to green")
                     motion.pick(perception.color_worldcoords["green"][0], perception.color_worldcoords["green"][1], perception.color_worldcoords["rot_g"])
+                    print("Have green")
                     motion.place(perception.color_worldcoords["red"][0],perception.color_worldcoords["red"][1] ,9)
-                    motion.b_pick(perception.color_worldcoords["blue"][0], perception.color_worldcoords["blue"][1]-0.5, perception.color_worldcoords["rot_b"])
+                    motion.b_pick(perception.color_worldcoords["blue"][0], perception.color_worldcoords["blue"][1]-1, perception.color_worldcoords["rot_b"])
                     motion.basket()
                     time.sleep(10)
                     perception.reset()
